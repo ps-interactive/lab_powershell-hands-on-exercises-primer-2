4e8879817442d787793ef7bc2958d6dfc547d7ef
Liam Cleary <liamcleary@msn.com>
2023-10-10 14:09:50 -0400

    Update Challenge-3.ps1

--- Challenge 3/Challenge-3.ps1
+++ Challenge 3/Challenge-3.ps1
@@ -1,7 +1,8 @@
-# Key Step 1: Identify and list all existing partitions on the computer.
+# Step 1: Identify and list all existing partitions on the computer.
 $partitions = Get-Volume
+$partitions
 
-# Key Step 2: Extract basic metadata for each partition.
+# Step 2: Extract basic metadata for each partition.
 foreach ($partition in $partitions) {
     $partitionInfo = [PSCustomObject]@{
         Name       = $partition.FileSystemLabel
@@ -10,7 +11,7 @@ foreach ($partition in $partitions) {
     Write-Output "Partition Name: $($partitionInfo.Name); File System: $($partitionInfo.FileSystem)"
 }
 
-# Key Step 3, 4 & 5: For each partition, determine the total, used, and available space.
+# Step 3: For each partition, determine the total, used, and available space.
 $partitionSpaces = foreach ($partition in $partitions) {
     $totalSpace = $partition.Size
     $freeSpace  = $partition.SizeRemaining
@@ -24,25 +25,104 @@ $partitionSpaces = foreach ($partition in $partitions) {
     }
 }
 
-# Displaying the computed spaces
+# Step 4: View the results
 $partitionSpaces | Format-Table -AutoSize
 
-# Key Step 6: Set up a mechanism to periodically retrieve storage metrics.
-# Assuming a 10 seconds interval for the purpose of the example. Adjust as needed.
+# Step 5: Set up a mechanism to periodically retrieve storage metrics.
 $intervalInSeconds = 10
 while ($true) {
     Start-Sleep -Seconds $intervalInSeconds
-
-    # Re-fetch partitions and recompute metrics
     $partitions = Get-Volume
-    # ... (Reuse the logic from the above steps for space calculation here if you wish to display it periodically)
+    $partitionSpaces = foreach ($partition in $partitions) {
+        $totalSpace = $partition.Size
+        $freeSpace  = $partition.SizeRemaining
+        $usedSpace  = $totalSpace - $freeSpace
+        [PSCustomObject]@{
+            PartitionName = $partition.FileSystemLabel
+            TotalSpace    = $totalSpace
+            UsedSpace     = $usedSpace
+            FreeSpace     = $freeSpace
+        }
+    }
+    $partitionSpaces | Format-Table -AutoSize
 }
 
-# Key Step 7: Implement logic to compare current storage status against predefined thresholds.
-$thresholdInPercent = 10 # For instance, alert if free space is below 10%. Adjust as needed.
+# Step 6: List Elapsed Timing
+$intervalInSeconds = 10
+$totalElapsedSeconds = 0
+while ($true) {
+    $startTime = Get-Date
+    Start-Sleep -Seconds $intervalInSeconds
+    $endTime = Get-Date
+    $elapsedTime = $endTime - $startTime
+    $elapsedSeconds = [math]::Round($elapsedTime.TotalSeconds)
+    $totalElapsedSeconds += $elapsedSeconds
+    # Display total elapsed time
+    Write-Host "Total Elapsed Seconds: $totalElapsedSeconds"
+    # Re-fetch partitions and recompute metrics
+    $partitions = Get-Volume
+    $partitionSpaces = foreach ($partition in $partitions) {
+        $totalSpace = $partition.Size
+        $freeSpace  = $partition.SizeRemaining
+        $usedSpace  = $totalSpace - $freeSpace
+        [PSCustomObject]@{
+            PartitionName = $partition.FileSystemLabel
+            TotalSpace    = $totalSpace
+            UsedSpace     = $usedSpace
+            FreeSpace     = $freeSpace
+        }
+    }
+    $partitionSpaces | Format-Table -AutoSize
+}
 
-foreach ($space in $partitionSpaces) {
-    if (($space.FreeSpace / $space.TotalSpace) * 100 -lt $thresholdInPercent) {
-        Write-Warning "Warning! Partition $($space.PartitionName) has less than $thresholdInPercent% free space left!"
+# Step 7: Add Message Logic
+$intervalInSeconds = 10
+while ($true) {
+    Start-Sleep -Seconds $intervalInSeconds
+    # Re-fetch partitions and recompute metrics
+    $partitions = Get-Volume
+    $partitionSpaces = foreach ($partition in $partitions) {
+        $totalSpace = $partition.Size
+        $freeSpace  = $partition.SizeRemaining
+        $usedSpace  = $totalSpace - $freeSpace
+        [PSCustomObject]@{
+            PartitionName = $partition.FileSystemLabel
+            TotalSpace    = $totalSpace
+            UsedSpace     = $usedSpace
+            FreeSpace     = $freeSpace
+        }
+    }
+    $partitionSpaces | Format-Table -AutoSize
+    $thresholdInPercent = 10
+    foreach ($space in $partitionSpaces) {
+        if ($space.TotalSpace -gt 0 -and ($space.FreeSpace / $space.TotalSpace) * 100 -lt $thresholdInPercent) {
+            Write-Warning "Warning! Partition $($space.PartitionName) has less than $thresholdInPercent% free space left!"
+        }
+    }
+}
+
+# Adjust the $thresholdInPercent to 90
+$intervalInSeconds = 10
+while ($true) {
+    Start-Sleep -Seconds $intervalInSeconds
+    # Re-fetch partitions and recompute metrics
+    $partitions = Get-Volume
+    $partitionSpaces = foreach ($partition in $partitions) {
+        $totalSpace = $partition.Size
+        $freeSpace  = $partition.SizeRemaining
+        $usedSpace  = $totalSpace - $freeSpace
+        [PSCustomObject]@{
+            PartitionName = $partition.FileSystemLabel
+            TotalSpace    = $totalSpace
+            UsedSpace     = $usedSpace
+            FreeSpace     = $freeSpace
+        }
+    }
+    $partitionSpaces | Format-Table -AutoSize
+    $thresholdInPercent = 90
+    foreach ($space in $partitionSpaces) {
+        if ($space.TotalSpace -gt 0 -and ($space.FreeSpace / $space.TotalSpace) * 100 -lt $thresholdInPercent) {
+            Write-Warning "Warning! Partition $($space.PartitionName) has less than $thresholdInPercent% free space left!"
+        }
     }
 }
