6591bd633a3c26239911434b51057412e20bb2cb
Liam Cleary <liamcleary@msn.com>
2023-10-11 15:05:53 -0400

    Update Challenge-5.ps1

--- Challenge 5/Challenge-5.ps1
+++ Challenge 5/Challenge-5.ps1
@@ -1,20 +1,38 @@
-# Key Step 2: Store this information in variables for further processing.
-# Key Step 5: Define variables for acceptable and critical CPU usage thresholds.
-$acceptableThreshold = 70  # e.g., 70%. Adjust as needed.
-$criticalThreshold = 90    # e.g., 90%. Adjust as needed.
-$logFile = "C:\Path\To\LogFile.txt"  # Define the path to your log file
+# Step 1: Define Variables
+$acceptableThreshold = 5 
+$criticalThreshold = 20
+$logFile = "C:\Users\public\Desktop\LAB_FILES\CPULogFile.txt" 
 
-# Key Step 3: Design a script to fetch and log CPU metrics at regular intervals.
-# Key Step 4: Utilize PowerShell's built-in logging functions.
-# Key Step 6: Implement logic.
+# Step 2: Create the LogFile
+New-Item -Path $logFile -ItemType File -Force
+
+# Step 3: Retrieve CPI Usage Data
+$cpuLoad = Get-Counter '\Processor(_Total)\% Processor Time' | ForEach-Object { $_.CounterSamples.CookedValue }
+$cpuLoad
+
+# Step 4: Retrieve Counter Sets
+Get-Counter -ListSet * | Select-Object CounterSetName
+
+# Step 5: Retrieve Counter Paths
+Get-Counter -ListSet * | Select-Object Paths
+
+# Step 6: Log CPU Usage Data
+Add-Content -Path $logFile -Value "[$(Get-Date)] CPU Load: $cpuLoad%"
+
+# Step 7: Create a Function to Log CPU Usage
 function LogCpuUsage {
-    # Key Step 1: Retrieve real-time CPU usage data.
     $cpuLoad = Get-Counter '\Processor(_Total)\% Processor Time' | ForEach-Object { $_.CounterSamples.CookedValue }
-    
-    # Log the CPU usage
+    Add-Content -Path $logFile -Value "[$(Get-Date)] CPU Load: $cpuLoad%"
+}
+
+LogCpuUsage
+
+
+# Step 8: Modify the Function to Compare Against Thresholds
+function LogCpuUsage {
+    $cpuLoad = Get-Counter '\Processor(_Total)\% Processor Time' | ForEach-Object { $_.CounterSamples.CookedValue }
     Add-Content -Path $logFile -Value "[$(Get-Date)] CPU Load: $cpuLoad%"
 
-    # Compare against thresholds
     if ($cpuLoad -gt $criticalThreshold) {
         Add-Content -Path $logFile -Value "[$(Get-Date)] ALERT: CPU Load is CRITICAL at $cpuLoad%."
     } elseif ($cpuLoad -gt $acceptableThreshold) {
@@ -22,10 +40,17 @@ function LogCpuUsage {
     }
 }
 
-# Key Step 7: Run the developed script to monitor CPU usage.
-$intervalInSeconds = 10  # Adjust the monitoring interval as needed
+LogCpuUsage
 
+# Step 9: Execute the Retrieval of CPU Usage Data, Log the Data, and Compare Against Thresholds, every 10 seconds
 while ($true) {
     LogCpuUsage
-    Start-Sleep -Seconds $intervalInSeconds
+    Start-Sleep -Seconds 10
+}
+
+# Step 10: xecute the Retrieval of CPU Usage Data, Log the Data, and Compare Against Thresholds, every 10 seconds and dssplay the results
+while ($true) {
+    LogCpuUsage
+    Start-Sleep -Seconds 10
+    Get-Content -Path $logFile
 }
